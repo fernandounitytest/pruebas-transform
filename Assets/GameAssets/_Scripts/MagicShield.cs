97ad93888b1d454333b67e0e15a8edbdb987ab6b
fernandopaniagua <fernando.paniagua@gmail.com>
2018-05-23 07:33:14 +0200

    Commit inicial
    Commit del día 22 de mayo

---
+++ Assets/GameAssets/_Scripts/MagicShield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicShield : MonoBehaviour {
+
+    public GameObject target;//Objeto sobre el que va a dar vueltas
+    public float rotationSpeed=360f;//Velocidad de rotación (grados por segundo)
+    public float distanceToTarget = 5;
+
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(target.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        //Calculo la distancia
+        Vector3 directionFromTarget = this.transform.position - target.transform.position;
+        //Elimino la componente y para que no se tenga en cuenta la altura de la bola en el cálculo
+        directionFromTarget.y = 0;
+        //Normalizo el vector
+        directionFromTarget.Normalize();
+        this.transform.position = target.transform.position + directionFromTarget * distanceToTarget;
+	}
+}
---
+++ Assets/GameAssets/_Scripts/TankMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+    public float speed = 10;//Metros por segundo
+    public float rotationSpeed = 90;//Grados por segundos
+    public GameObject target;
+    public GameObject turretBone;
+    public GameObject cannonBone;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+        //Desplazamiento con coorenadas locales
+        //this.transform.position += 
+        //    this.transform.forward * Time.deltaTime * speed * vInput;
+        //Desplazamiento con coordenadas globales pero convierte las coordenadas a local
+        this.transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
+
+        //Rotación con coordenadas globales
+        this.transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime * hInput, 0));
+        //OTRA OPCIÓN PARA LO MISMO
+        //this.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        Vector3 targetPosition = target.transform.position;
+        Vector3 turretTargetPosition = targetPosition;
+        turretTargetPosition.y = turretBone.transform.position.y;
+        turretBone.transform.LookAt(turretTargetPosition);
+
+        cannonBone.transform.LookAt(targetPosition);
+	}
+}
