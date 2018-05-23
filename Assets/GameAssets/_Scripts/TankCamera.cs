037fd66f0f2a64bf7eb1c462fefa867026cae143
fernandopaniagua <fernando.paniagua@gmail.com>
2018-05-23 11:57:25 +0200

    Tres tanques que disparan

---
+++ Assets/GameAssets/_Scripts/TankCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCamera : MonoBehaviour {
+    public GameObject target;
+    public float sensitivity = 0.5f;
+    public Vector3 followDistance = new Vector3(0, 20, -20);
+
+	// Use this for initialization
+	void Start () {
+        //OPCION 1: Con coordenadas globales
+        //this.transform.position = target.transform.position + followDistance;
+
+        //OPCION 2: Con desplazamientos sobre la posición del target
+        /*
+        this.transform.position = target.transform.position +
+            target.transform.forward * followDistance.z +
+            target.transform.up * followDistance.y + 
+            target.transform.right * followDistance.x;
+        this.transform.LookAt(target.transform);
+        this.transform.SetParent(target.transform);
+        */
+
+        //OPCION 3, NO NECESITA EL CÓDIGO DE LATE UPDATE
+        this.transform.SetParent(target.transform);
+        this.transform.localPosition = followDistance;
+        this.transform.LookAt(target.transform);
+	}
+
+    private void Update()
+    {
+        //GetMouseButton(1) se corresponde con el click derecho
+        if (Input.GetMouseButton(1))
+        {
+            float xInput = Input.GetAxis("Mouse X");
+            this.transform.RotateAround(
+                point:target.transform.position, 
+                axis:Vector3.up, 
+                angle:xInput * sensitivity);
+        }
+
+    }
+
+    // Si lo hiciésemos en Update, petardea
+    /*
+	void LateUpdate () {
+        this.transform.position = target.transform.position +
+            target.transform.forward * followDistance.z +
+            target.transform.up * followDistance.y +
+            target.transform.right * followDistance.x;
+        this.transform.LookAt(target.transform);
+    }
+    */
+}
--- Assets/GameAssets/_Scripts/TankMovement.cs
+++ Assets/GameAssets/_Scripts/TankMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class TankMovement : MonoBehaviour {
     public float speed = 10;//Metros por segundo
     public float rotationSpeed = 90;//Grados por segundos
+
     public GameObject target;
     public GameObject turretBone;
     public GameObject cannonBone;
+
+    public Rigidbody bulletPrefab;
+    public GameObject shootPoint;
+    public float shootForce;
 
 	// Use this for initialization
 	void Start () {
@@ -18,22 +23,56 @@ public class TankMovement : MonoBehaviour {
 	void Update () {
         float vInput = Input.GetAxis("Vertical");
         float hInput = Input.GetAxis("Horizontal");
+        bool shootInput = Input.GetButtonDown("Fire1");//Disparo
+
+        MoveTank(vInput);
+        RotateTank(hInput);
+
+        RotateTurretToTarget();
+        RotateCannonToTarget();
+        Firecannon(shootInput);
+	}
+
+    private void MoveTank(float vInput)
+    {
         //Desplazamiento con coorenadas locales
         //this.transform.position += 
         //    this.transform.forward * Time.deltaTime * speed * vInput;
         //Desplazamiento con coordenadas globales pero convierte las coordenadas a local
         this.transform.Translate(Vector3.forward * Time.deltaTime * speed * vInput);
+    }
 
+    private void RotateTank(float hInput)
+    {
         //Rotación con coordenadas globales
         this.transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime * hInput, 0));
         //OTRA OPCIÓN PARA LO MISMO
         //this.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
 
+    private void RotateTurretToTarget()
+    {
         Vector3 targetPosition = target.transform.position;
         Vector3 turretTargetPosition = targetPosition;
         turretTargetPosition.y = turretBone.transform.position.y;
         turretBone.transform.LookAt(turretTargetPosition);
+    }
 
+    private void RotateCannonToTarget()
+    {
+        Vector3 targetPosition = target.transform.position;
         cannonBone.transform.LookAt(targetPosition);
-	}
+    }
+
+    private void Firecannon(bool shootInput)
+    {
+        if (shootInput)
+        {
+            Rigidbody newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.position = shootPoint.transform.position;
+            newBullet.transform.rotation = shootPoint.transform.rotation;
+            newBullet.AddForce(shootPoint.transform.forward * shootForce, ForceMode.Impulse);
+            Destroy(newBullet.gameObject, 10);
+        }
+    }
 }
