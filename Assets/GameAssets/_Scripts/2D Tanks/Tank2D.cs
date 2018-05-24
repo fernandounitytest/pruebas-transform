d02c864679208e9a81935f6981f67cdb781c6ebe
fernandopaniagua <fernando.paniagua@gmail.com>
2018-05-25 01:27:35 +0200

    Sesion 24 mayo - parcial

---
+++ Assets/GameAssets/_Scripts/2D Tanks/Tank2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank2D : MonoBehaviour {
+    private Tile2D targetTile;
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetTile != null)
+        {
+            //MoveToTileWithTranslate();
+            MoveToTileWithMoveTowards();
+        }
+    }
+
+    private void MoveToTileWithTranslate()
+    {
+        //this.transform.position = targetTile.transform.position;//Teletransporte
+        //this.transform.LookAt(targetTile.transform);//Mira hacia el target
+
+        if ((Vector3.Distance(this.transform.position, targetTile.transform.position)) > 0.1f)
+        {
+            Vector3 directionToTile = targetTile.transform.position - this.transform.position;
+            directionToTile.Normalize();
+            this.transform.Translate(directionToTile * Time.deltaTime * speed);
+        }
+    }
+
+    private void MoveToTileWithMoveTowards()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, targetTile.transform.position, 0.1f);
+    }
+
+    public void MoveToTile(Tile2D tile)
+    {
+        targetTile = tile;
+    }
+}
---
+++ Assets/GameAssets/_Scripts/2D Tanks/Tile2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile2D : MonoBehaviour {
+    private Tank2D tank;
+	// Use this for initialization
+	void Start () {
+        tank = GameObject.Find("Tank").GetComponent<Tank2D>();
+        Debug.Log("El tanque se llama:" + tank.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseUpAsButton()
+    {
+        Debug.Log("He pulsado sobre la casilla " + this.name);
+        tank.MoveToTile(this);
+    }
+
+    /*
+    //Se mueve en direccion al raton
+    private void OnMouseOver()
+    {
+        tank.MoveToTile(this);
+    }
+    */
+}
